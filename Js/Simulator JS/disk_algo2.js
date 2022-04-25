e85deee57fba4ae83cfb2f73b2fe1cc6c30e4d26
Shivani8202 <93913320+Shivani8202@users.noreply.github.com>
2022-04-25 21:21:59 +0530

    Update disk_algo2.js

--- Js/Simulator JS/disk_algo2.js
+++ Js/Simulator JS/disk_algo2.js
@@ -6,77 +6,106 @@ function autoGenerate()
     x.value += i + " ";
 }
 
-function Calculate() {
-    let errorMessage = document.getElementById('errorMessage');  
+function Calculate()
+{
+    var errorMessage = document.getElementById('errorMessage');  
     hideError(errorMessage);
 
     //Getting numbers(data)from user input
-    let inputNumbersString = document.getElementById('inputNumbers').value;
+    var inputNumbersString = document.getElementById('inputNumbers').value;
     inputNumbersString = inputNumbersString.trim();
-    let inputNumbers = inputNumbersString.split(" ");
-    let inputHeadPos = document.getElementById('inputHeadPos').value;
+    var inputNumbers = inputNumbersString.split(" ");
+    var inputHeadPos = document.getElementById('inputHeadPos').value;
 
     //Removing header values from user input
-    for (let i = 0; i < inputNumbers.length; i++) {
-        if (inputNumbers[i] == inputHeadPos) {
+    for (var i = 0; i < inputNumbers.length; i++)
+    {
+        if (inputNumbers[i] == inputHeadPos)
+        {
             inputNumbers.splice(i, 1);
         }
     }
     inputNumbers.unshift(inputHeadPos);
 
     //Removing the duplicate values
-    inputNumbers = inputNumbers.filter(function(item, pos) {
+    inputNumbers = inputNumbers.filter(function (item, pos)
+    {
         return inputNumbers.indexOf(item) == pos;
     });
 
     //Validation
-    let isValidInput = true; 
- 
+    var isValidInput = true; 
+    
     if (inputHeadPos == "") {
-        showError(errorMessage, "Please enter current Head position");
+        //displaying error if occurred
+        showError(errorMessage, "Current Head position is required");
         isValidInput = false;
-    } else if (isNaN(inputHeadPos)) {
+    }
+
+    //if value of inputHeadPos is not a number
+    else if (isNaN(inputHeadPos)) {
+        //displaying error if occurred
         showError(errorMessage, "Only Numeric value allowed for current Head position !!!");
-        isValidInput = false; 
-    } else if (parseInt(inputHeadPos) < 0 || parseInt(inputHeadPos) > 199) {
-        showError(errorMessage, "Current Head position value must be in between 1-199");
         isValidInput = false;
-    } else if (inputNumbersString == "") {
+    }
+
+    //if value of inputHeadPos is negative
+    else if (parseInt(inputHeadPos) < 0) {
+        showError(errorMessage, "Current Head position must be positive integer");
+        isValidInput = false;
+    }
+
+    //if value of userInputQueue is empty
+    else if (inputNumbersString == "") {
+        //displaying error if occurred
         showError(errorMessage, "Numeric values required for Queue");
-        isValidInput = false; 
+        isValidInput = false;
     }
     else {
-        inputNumbers.forEach(number => {
-            if (isNaN(number)) {
+        var totalNumbers = inputNumbers.length;
+
+        //using for loop to check every input of inputNumbers
+        for (var i = 0; i < totalNumbers; i++) {
+
+            //if values of inputNumbers is not a number
+            if (isNaN(inputNumbers[i])) {
+                //displaying error if occurred
                 showError(errorMessage, "Number queue must only contain numbers");
                 isValidInput = false;
-            } else if (parseInt(number) < 0 || parseInt(number) > 199) {
-                showError(errorMessage, "Number queue values must be in the range of 1-199");
+            }
+
+            //if values of inputNumbers is negative
+            else if (parseInt(inputNumbers[i]) < 0) {
+                //displaying error if occurred
+                showError(errorMessage, "Number queue values must be positive integer");
                 isValidInput = false;
             }
-        });
+        }
     }
+
+    if(isValidInput){
     // var graphType = document.getElementById("chartType").value;
     var ctx = document.getElementById("line-chart").getContext('2d');
     var lineChart = new Chart(ctx, {
         type: 'line',
         data: {
-            labels: sstf(inputNumbers, inputHeadPos),
+            labels: fcfs(inputNumbers, inputHeadPos),
             legend: {
                 display: true
             },
             datasets: [
                 {
-                    label: "Shortest Seek Time First (SSTF) Algorithm",
-                    data: seekAddressed,
+                    label: "First Come First Serve (FCFS) Algorithm",
+                    data: inputNumbers,
                     lineTension: 0,
                     fill: false,
-                    backgroundColor: "rgba(0,255,230, 0.8)",
-                    borderColor: "rgba(0,255,230, 1)",
-                    pointBackgroundColor: "rgba(0,255,230, 0.5)",
+                    backgroundColor: "rgba(0,178,255, 0.8)",
+                    borderColor: "rgba(0,178,255, 1)",
+                    pointBackgroundColor: "rgba(0,178,255, 0.6)",
                     pointBorderColor: "#55bae7",
                     pointHoverBackgroundColor: "#55bae7",
                     pointHoverBorderColor: "#55bae7",
+                    borderWidth: 3,
                 }
             ]
         },
@@ -110,6 +139,7 @@ function Calculate() {
         printImage();
     });
 }
+} 
 
 function printImage()
 {
@@ -117,7 +147,7 @@ function printImage()
     var canvas_img = canvas.toDataURL("image/png",1.0);
     var pdf = new jsPDF('landscape','in', 'letter');
     pdf.addImage(canvas_img, 'png', .5, 1.75, 10, 5);
-    pdf.save('SSTF Chart.pdf');
+    pdf.save('FCFS Chart.pdf');
 };
 
 function showError(errorMessage, msg)
@@ -141,50 +171,30 @@ function showResult(count, seekSequence)
     else div.innerHTML = "<br/>Seek Sequence: <b>[" + seekSequence + "]</b><br /><br/>Total Seek Count: <b>" + count + "<b>";
 }
 
-var seekCountSequence = [];
-var seekAddressed = [];
-
-function sstf(Numbers, Head)
+function fcfs(Numbers, Head)
 {
     var tempArray = [];
-    var visited = [];
-    var totalNumbers = Numbers.length;
+    var seekCountSequence = [];
+    var totalNumbersLength = Numbers.length;
     var totalHeadMovements = 0;
     var distance = 0;
-    var temp;
 
-    for (var i = 0; i < totalNumbers; i++)
+    for (var i = 0; i < totalNumbersLength; i++)
     {
         Numbers[i] = parseInt(Numbers[i]);
     }
-    
-    for (var i = 0; i < totalNumbers; i++)
-    {
-        visited.push(0);
-    }
-    temp = parseInt(Head);
 
-    for (var i = 0; i < totalNumbers; i++)
+    for (var i = 0; i < totalNumbersLength; i++)
     {
-        var minValue = 1000000;
-        var index;
-        for (var j = 0; j < totalNumbers; j++)
-        {
-            if (Math.abs(temp - Numbers[j]) < minValue && (visited[j] === 0))
-            {
-                index = j;
-                minValue = Math.abs(temp - Numbers[j]);
-            }
-        }
-        totalHeadMovements += Math.abs(temp - Numbers[index]);
+        var currentTrack = Numbers[i];
+        distance = Math.abs(currentTrack - Head);
+        totalHeadMovements += distance;
         seekCountSequence.push(totalHeadMovements);
-        visited[index] = 1;
-        temp = Numbers[index];
-        seekAddressed.push(Numbers[index]);
+        Head = currentTrack;
     }
-    for (var i = 1; i < totalNumbers; i++)
+    for (var i = 1; i < totalNumbersLength; i++)
     {
-        tempArray.push(seekAddressed[i]);
+        tempArray.push(Numbers[i]);
     }
     showResult(totalHeadMovements, tempArray);
     return seekCountSequence;
